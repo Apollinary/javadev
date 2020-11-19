e02c333baad1f0855e95e706f22b7e20546539bd
Apollinary <apollinariiuvarov@mail.ru>
2020-11-19 19:16:00 +0300

    SpringBootKafka
    Initial commit

---
+++ SpringBootKafkaApplication/src/main/java/org/example/KafkaSenderExample.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+class KafkaSenderExample {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaSenderExample(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    void sendMessage(String topicName, String message ) {
+        kafkaTemplate.send(topicName, message);
+        System.out.println("Sent successfully");
+    }
+
+}
